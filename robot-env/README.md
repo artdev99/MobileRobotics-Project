44b47615fd9060493bd25cbd5d20c5f52ca76ade
artdev99 <48352052+artdev99@users.noreply.github.com>
2024-11-13 10:15:19 +0100

    drawio env

--- robot-env/README.md
+++ robot-env/README.md
@@ -5,3 +5,22 @@
 - robot/start: white
 
 Page format: A3
+
+# draw.io
+- Diagram Menu
+    - Background Color: Black
+    - Paper Size: A3
+    - Paper Orientation: Landscape
+- View Menu
+    - Units
+        - Milimeters
+- Select Delay
+    - Arange Menu
+    - Size 36mm 36mm
+
+Using delay shape is a good approx for thymio robot:
+![Approx](https://i.postimg.cc/bw3hj8wb/image-2024-11-13-100740839.png)
+
+# File name convention
+- `rx.png` where no robot is present, real life purposes
+- `sx.png` where robot is present, simulation purposes
---
+++ robot-env/size_converter.py
@@ -0,0 +1,24 @@
+# A3 landscape width  420mm
+# A3 landscape height 297mm
+# Aspect ratio 1.414141 (420/297) == (width/height)
+a3_width = 420 #mm
+a3_height = 297 #mm
+
+# laptop screen
+screen_width = 139.0 #mm
+screen_height =  screen_width / (420.0 / 297.0)
+
+# IRL
+robot_width = 110 #mm
+robot_height = 110 #mm
+robot_height_1 = 85 #mm
+robot_height_2 = 25 #mm
+
+# 420 = 139
+# 110 = ?
+
+screen_robot_width = int(robot_width * screen_width / a3_width)
+screen_robot_height_1 = int(robot_height_1 * screen_height / a3_height)
+screen_robot_height_2 = int(robot_height_2 * screen_height / a3_height)
+
+print(screen_robot_width, screen_robot_height_1, screen_robot_height_2)
