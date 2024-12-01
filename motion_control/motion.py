901de571f3f0ddb03cc38b99c65d92b574f5d780
amandine-mnr <amandine.meunier@epfl.ch>
2024-12-01 19:23:05 +0100

    debug motion

--- motion_control/motion.py
+++ motion_control/motion.py
@@ -41,36 +41,40 @@ def motion_control(x,y,theta,x_goal,y_goal):
     
     #To do : Tune k by testing
     k_rho = 1.0     #controls translational velocity
-    k_alpha = 3.0   #controls rotational velocity 
-    k_beta = -0.5   #damping term (to stabilize the robot's orientation when reaching the goal)
+    k_alpha = 1.0   #controls rotational velocity 
+    k_beta = 0      #damping term (to stabilize the robot's orientation when reaching the goal)
 
     delta_x = x_goal - x #[cm]
     delta_y = y_goal - y #[cm]
 
     distance_to_goal =math.sqrt( (delta_x)**2 + (delta_y)**2 )          #[cm]
     delta_angle = normalize_angle(math.atan2(delta_y, delta_x) - theta) #difference between the robot's orientation and the direction of the goal [rad]
-    #print("IN: d=", distance_to_goal, " angle=",delta_angle)
+    print("IN: d=", distance_to_goal, " angle=",math.degrees(delta_angle))
 
     v = k_rho*distance_to_goal                                  #translational velocity [cm/s]
     omega = k_alpha*(delta_angle) - k_beta*(delta_angle+theta)  #rotational velocity [rad/s]
+    print("omega = ", omega)
     
     #Calculate motor speed
     w_ml = (v+omega*L_AXIS)/R_WHEEL #[rad/s]
     w_mr = (v-omega*L_AXIS)/R_WHEEL #[rad/s]
-    #print("IN: w_ml, w_mr : ", w_ml, w_mr)
+    print("after omega: w_ml, w_mr : ", w_ml, w_mr)
     
     v_ml = w_ml*SCALING_FACTOR #PWM
     v_mr = w_mr*SCALING_FACTOR #PWM
-    #print("IN: v_ml, v_mr : ", v_ml, v_mr)
+    #print("before: v_ml, v_mr : ", v_ml, v_mr)
 
     if(goal_reached(distance_to_goal,delta_angle)):
         v_ml=0
         v_mr=0
         print("goal reached !")
     
+    v_ml = v_ml/10
+    v_mr = v_mr/10
+    
     v_ml = limit_speed(v_ml)
     v_mr = limit_speed(v_mr)
-    print("IN: v_ml, v_mr : ", v_ml, v_mr)
+    #print("IN: v_ml, v_mr : ", v_ml, v_mr)
     
     v_ml = int(v_ml)  #ensure integer type
     v_mr = int(v_mr)  #ensure integer type
